56d3ca7d5369e13c045269270c559ca94ed283a8
Dylan SARDI <dylan.sardi@gmail.com>
2021-11-19 18:09:23 +0100

    ajout d'une classe de connexion à la bdd

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -3,11 +3,12 @@
 namespace App\Controller\Front;
 
 use App\Controller\AbstractController;
-
+use App\Database\DBConnection;
 class HomeController extends AbstractController
 {
     public function __invoke()
     {
+        $db = new DBConnection($_ENV['DATABASE_NAME'], $_ENV['DATABASE_HOST'], $_ENV['DATABASE_USER'], $_ENV['DATABASE_PASSWORD']);
         $this->render('homepage.twig', 'Front');
     }
 }
