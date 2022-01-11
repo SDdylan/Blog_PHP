5e80f3fbbbaa9b39b9bde07619e0f4cb821fe2b6
Dylan SARDI <dylan.sardi@gmail.com>
2022-01-11 12:07:02 +0100

    Access to the admin pages facilitated. Added buttons to go back to previous page for commentPost.twig and addpost.twig.

--- src/Controller/Admin/PostCommentController.php
+++ src/Controller/Admin/PostCommentController.php
@@ -4,12 +4,14 @@ namespace App\Controller\Admin;
 
 use App\Exception\PostNotFoundException;
 use App\Repository\CommentRepository;
+use App\Repository\PostRepository;
 
 class PostCommentController extends AdminController
 {
     public function __invoke(array $parameters)
     {
         $postId = (int) $parameters['postId'];
+        $post = PostRepository::getPost($postId);
         try {
             if (isset($_POST["status-form"])) {
                 var_dump($_POST["comment-id"]);
@@ -17,7 +19,7 @@ class PostCommentController extends AdminController
                 $commentStatus = CommentRepository::changeStatusComment($_POST["comment-id"], $_POST["comment-status"]);
             }
             $comments = CommentRepository::getCommentsPost($postId);
-            $this->render('commentPost.twig', 'Admin', ['comments' => $comments]);
+            $this->render('commentPost.twig', 'Admin', ['comments' => $comments, 'post' => $post]);
         } catch (PostNotFoundException $exception) {
             $this->redirectToUrl();
         }
